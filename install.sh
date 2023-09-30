91b8b3904b50ddaee77e4c823d6c5aeb57e2cfbe
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2023-10-01 00:05:15 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ termux-setup-storage
 # pkgs
 apt update && apt upgrade -y
 apt update && apt install nala -y
-nala install zsh python wget gh python-pip lazygit luarocks luajit ripgrep fd gettext neovim nodejs git micro openssh zip curl ruby timewarrior taskwarrior htop figlet logo-ls lsd -y || error_exit "${RED}Failed to install packages.${NC}"
+nala install zsh python wget gh python-pip lazygit luarocks luajit ripgrep fd gettext neovim nodejs git micro openssh zip curl ruby timewarrior taskwarrior htop figlet termux-api logo-ls lsd -y || error_exit "${RED}Failed to install packages.${NC}"
 nala install golang php rust openjdk-17 
 # font
   cd ~/.termux
