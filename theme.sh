0a2a4e78863a34abfee248171728a7c4bca1c1dd
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2024-10-06 23:44:57 +0530

    update theme.sh

--- theme.sh
+++ theme.sh
@@ -1,9 +1,10 @@
-#pkg   
-pkg install nala git -y
+#pkg 
 cd $PREFIX/etc/apt
    rm -rf sources.list
    echo "deb https://packages.termux.dev/apt/termux-main stable main" >> sources.list
-   
+     
+pkg install nala git -y
+
 cd $HOME
 git clone https://github.com/tharindu899/x-theme
 mv ~/x-theme/font.ttf ~/.termux
